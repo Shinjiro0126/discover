48270522d6a05256e47b714be5008349d44d1348
Shinjiro0126 <mailac63chiokc35@gmail.com>
2020-08-27 00:03:04 +0900

    tweet_model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,16 @@
 class Tweet < ApplicationRecord
+  validates :text, presence: true
+  validates :image, presence: true
+  
+  belongs_to :user
+
+  mount_uploader :image, ImageUploader
+
+  def self.search(search)
+    if search 
+      Tweet.where('text LIKE(?)', "%#{search}%")
+    else  
+      Tweet.all 
+    end  
+  end
 end
