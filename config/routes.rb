ed723aee41cf0910ed19fcb12f2b2f54cf6c19bb
Shinjiro0126 <mailac63chiokc35@gmail.com>
2020-08-27 15:55:06 +0900

    comments_route

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   
   root to: 'tweets#index'
   resources :tweets do 
+    resources :comments, only: :create
     collection do 
       get 'search'
     end 
   end 
   resources :users, only: [:show, :edit, :update]
+  
 end
