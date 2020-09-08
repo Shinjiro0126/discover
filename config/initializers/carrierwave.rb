0bbfa9de270eb2c990039d974e35acbceee9c3f8
Shinjiro0126 <mailac63chiokc35@gmail.com>
2020-09-08 21:53:59 +0900

    edit_aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,14 +4,16 @@ require 'carrierwave/storage/fog'
 
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-      region: EVN['ap-northeast-1']
+      region: 'ap-northeast-1'
     }
-    config.fog_directory  = ENV['discover-0126']
+    config.fog_directory  = 'discover-0126'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/discover-0126'
   end
   
   CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/ 
