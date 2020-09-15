30b265a187e2fa158fbd8f024bac8e6a0e977f89
Shinjiro0126 <mailac63chiokc35@gmail.com>
2020-09-15 23:13:32 +0900

    create_hashtag_and_tweethashtag_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_125835) do
+ActiveRecord::Schema.define(version: 2020_09_15_141103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,11 +23,27 @@ ActiveRecord::Schema.define(version: 2020_09_07_125835) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "hashtags", force: :cascade do |t|
+    t.string "hashname"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["hashname"], name: "index_hashtags_on_hashname", unique: true
+  end
+
   create_table "likes", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "tweet_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tweet_hashtags", force: :cascade do |t|
+    t.bigint "tweet_id"
+    t.bigint "hashtag_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["hashtag_id"], name: "index_tweet_hashtags_on_hashtag_id"
+    t.index ["tweet_id"], name: "index_tweet_hashtags_on_tweet_id"
   end
 
   create_table "tweets", force: :cascade do |t|
@@ -55,4 +71,6 @@ ActiveRecord::Schema.define(version: 2020_09_07_125835) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tweet_hashtags", "hashtags"
+  add_foreign_key "tweet_hashtags", "tweets"
 end
